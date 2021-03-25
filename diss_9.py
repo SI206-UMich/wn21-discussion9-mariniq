0b7bca1bbe88413aebc4450a74debbb8a99f1ac9
Marini <mariniq@umich.edu>
2021-03-25 11:38:32 -0400

    done

--- diss_9.py
+++ diss_9.py
@@ -6,34 +6,45 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    anchor = soup.find("div", class_ = "infocard-list infocard-list-pkmn-lg")
+    anchor2 = anchor.find_all("span", class_ = "infocard-lg-img")[3]
+    anchor3 = anchor2.find("a")["href"]
+    return "https://pokemondb.net" + anchor3
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
     url = 'https://pokemondb.net/pokedex/'+pokemon
-    #add code here
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    movelist = []
+    anchor = soup.find_all("table", class_ = "data-table")[2]
+    anchor2 = anchor.find_all("a", class_ = "ent-name")
+    for x in anchor2:
+        movelist.append(x.text)
+    return movelist
+    
 
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
+
 def findLetters(sentences):
     # initialize an empty list
-    
+    timelist = []
 
     # define the regular expression
-    
+    expr = r"@((?:[1-9]|1[0-2])[ap]m)"
 
     # loop through each sentence or phrase in sentences
+    for s in sentences:
     
-
     # find all the words that match the regular expression in each sentence
-       
-
+        x = re.findall(expr, s)
     # loop through the found words and add the words to your empty list
-
+        for i in x:
+            timelist.append(i)
 
     #return the list of the last letter of all words that begin or end with a capital letter
-
+    return timelist
 
 
 def main():
@@ -55,7 +66,7 @@ class TestAllMethods(unittest.TestCase):
 
     def test_findLetters(self):
         self.assertEqual(findLetters(['Come eat lunch at 12','there"s a party @2pm', 'practice @7am','nothing']), ['2pm', '7am'])
-        self.assertEqual(findLetters(['There is show @12pm if you want to join','I will be there @ 2pm', 'come at @3 pm will be better']), ['12pm', '3 pm'])
+        self.assertEqual(findLetters(['There is show @12pm if you want to join','I will be there @ 2pm', 'come at @3pm will be better']), ['12pm', '3pm'])
 
 if __name__ == "__main__":
     main()
